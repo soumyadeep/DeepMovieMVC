5de090f72321a3e8ce3303806d2b15abe1ea6259
soumyadeepghoshdastidar <soumyadeep39@gmail.com>
2019-01-16 14:00:15 +0100

    Initial check-in of module DeepWebAppMVC

---
+++ Models/PagesMovieContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeepWebAppMVC.Models
+{
+    public class PagesMovieContext : DbContext
+    {
+        public PagesMovieContext(DbContextOptions<PagesMovieContext> options) : base(options)
+        {
+        }
+        public DbSet<DeepWebAppMVC.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DeepWebAppMVC.Models;
+
+namespace DeepWebAppMVC.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DeepWebAppMVC.Models.PagesMovieContext _context;
+
+        public CreateModel(DeepWebAppMVC.Models.PagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
